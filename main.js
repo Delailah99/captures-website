4c090b93e5398673f20f09f11eeef341ab427222
Delailah99 <158446805+Delailah99@users.noreply.github.com>
2024-03-04 16:03:50 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function submitForm() {
+    const firsName=document.getElementById("first-name").value
+    const lastName=document.getElementById("last-name").value
+    const email=document.getElementById("email").value
+    const message=document.getElementById("message").value
+    localStorage.setItem(Date.now().toString(),JSON.stringify( {
+    firstName: firsName,
+    lastName: lastName,
+    email: email,
+    message: message,
+  }));
+}
+
+function displaySubmissionCount(){
+    const submissionCount=localStorage.length
+    document.getElementById("submission-count").innerText=`Total submissions: ${submissionCount.toString()}`;
+    console.log("Hello");
+}
+window.onload=function(){
+displaySubmissionCount()
+}
+
